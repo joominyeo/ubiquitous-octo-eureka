b37daf27357756a7ce3cd907693b4b3734415d99
Cai Yeo <joominyeo@outlook.com>
2024-07-09 18:00:26 -0700

    hello world

---
+++ hello_world.c
@@ -0,0 +1,10 @@
+#include <iostream> // Includes the IO stream library
+
+int main()
+{
+  std::cout << "Hello, World!" << std::endl;
+  // Prints "Hello, World!" to the console
+ 
+  return 0;                                
+  // Indicates that the program ended successfully
+}
